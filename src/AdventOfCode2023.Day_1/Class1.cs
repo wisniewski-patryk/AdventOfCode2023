b58e6900a9db641796486fa8992b193ad7ea884d
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2023-12-01 11:42:58 +0100

    add gitignore

---
+++ src/AdventOfCode2023.Day_1/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace AdventOfCode2023.Day_1;
+
+public class Class1
+{
+
+}
---
+++ src/AdventOfCode2023.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AdventOfCode2023.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
