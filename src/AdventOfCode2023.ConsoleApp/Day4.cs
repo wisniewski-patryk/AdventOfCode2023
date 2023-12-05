fa9456544db50ce11ce7d7241af77e973726f09f
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2023-12-05 07:52:51 +0100

    Day4.1

---
+++ src/AdventOfCode2023.ConsoleApp/Day4.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode2023.Day_4;
+
+public class Day4
+{
+	public int Solution()
+	{
+		var input = File.ReadAllLines(@".\Inputs\inputPuzzle4.txt");
+		var sum = 0;
+		foreach (var line in input)
+		{
+			sum += GetScore(line);
+		}
+		return sum;
+	}
+
+	public int GetScore(string input)
+	{
+		var splitGameNumberFromNumbers = input.Split(':');
+		var splitWinnerNumbersFromPlayerNumbers = splitGameNumberFromNumbers[1].Trim().Split("|");
+		var winnerNumbers = splitWinnerNumbersFromPlayerNumbers[0].Trim().Split(' ').Select(s =>
+		{
+			_ = int.TryParse(s.Trim(), out var result);
+			return result;
+		}).Where(s => s != 0);
+		var playersNumbers = splitWinnerNumbersFromPlayerNumbers[1].Trim().Split(' ').Select(s =>
+		{
+			_ = int.TryParse(s.Trim(), out var result);
+			return result;
+		}).Where(s => s != 0);
+		var gamePreview = splitGameNumberFromNumbers[0];
+		var score = 0;
+		var machedNumber = new List<int>();
+
+		foreach (var winnerNumber in winnerNumbers)
+		{
+			foreach (var playersNumber in playersNumbers)
+			{
+				if (playersNumber == winnerNumber)
+				{
+					if (score == 0)
+					{
+						score = 1;
+						machedNumber.Add(playersNumber);
+					} else
+					{
+						score *= 2;
+						machedNumber.Add(playersNumber);
+					}
+				}
+			}
+		}
+
+		return score;
+	}
+}
--- src/AdventOfCode2023.ConsoleApp/Program.cs
+++ src/AdventOfCode2023.ConsoleApp/Program.cs
@@ -1,19 +1,34 @@
 ﻿using AdventOfCode2023.Day_1;
 using AdventOfCode2023.Day_2;
 using AdventOfCode2023.Day_3;
+using AdventOfCode2023.Day_4;
 
 Console.WriteLine("Hello, Advent of Code 2023!!");
+var currentDay = 4;
 
-var puzzle1 = new Day1();
-var solutionPuzzle1 = puzzle1.Solution();
-Console.WriteLine("Solution Puzzle 1.1: " + solutionPuzzle1);
-
-var puzzle2 = new Day2();
-var solutionPuzzle2 = puzzle2.Solution();
-Console.WriteLine("Solution Puzzle 2.1: " + solutionPuzzle2.sum);
-Console.WriteLine("Solution Puzzle 2.2: " + solutionPuzzle2.powerSum);
-
-var puzzle3 = new Day3();
-var solutionPuzzle3 = puzzle3.Solution();
-Console.WriteLine("Solution Puzzle 3.1: " + solutionPuzzle3);
-
+switch (currentDay)
+{
+	case 1:
+		var puzzle1 = new Day1();
+		var solutionPuzzle1 = puzzle1.Solution();
+		Console.WriteLine("Solution Puzzle 1.1: " + solutionPuzzle1);
+		break;
+	case 2:
+		var puzzle2 = new Day2();
+		var solutionPuzzle2 = puzzle2.Solution();
+		Console.WriteLine("Solution Puzzle 2.1: " + solutionPuzzle2.sum);
+		Console.WriteLine("Solution Puzzle 2.2: " + solutionPuzzle2.powerSum);
+		break;
+	case 3:
+		var puzzle3 = new Day3();
+		var solutionPuzzle3 = puzzle3.Solution();
+		Console.WriteLine("Solution Puzzle 3.1: " + solutionPuzzle3);
+		break;
+	case 4:
+		var puzzle4 = new Day4();
+		var solutionPuzzle4 = puzzle4.Solution();
+		Console.WriteLine("Solution Puzzle 4.1: " + solutionPuzzle4);
+		break;
+	default:
+		break;
+}
